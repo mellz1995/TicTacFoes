3208de5450911d9eb101c2328c33ceb381730957
Melvin L. Corners <mlcorner@go.olemiss.edu>
2016-12-31 09:17:35 -0600

    fixed image error. added music for test 4x4 test board

--- Tic Tac Foes/Test4x4ViewController.swift
+++ Tic Tac Foes/Test4x4ViewController.swift
@@ -49,11 +49,11 @@ class Test4x4ViewController: UIViewController {
     var activeGame = true
     
     //Winning Combinations
-    let winningCombinations = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15], [0, 4, 8, 12], [1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15], [0, 5, 10, 15]]
+    let winningCombinations = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15], [0, 4, 8, 12], [1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15], [0, 5, 10, 15], [3, 6, 9, 12]]
     
     @IBAction func buttonPressed(_ sender: AnyObject) {
         buttonPressedSound?.play()
-        
+        print(sender.tag)
         let activePosition = sender.tag - 1
         
         if gameState[activePosition] == 0 && activeGame{
@@ -80,6 +80,7 @@ class Test4x4ViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        battleMusic?.play()
     }
 
     override func didReceiveMemoryWarning() {
